305b3d23c88d34041a7b14e1464e1f934f1ac4b3
Fachri Reza <fachri.reza@sirclo.com>
2024-04-17 14:41:48 +0700

    modal & serviceworker

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Modal = ({handleShowModal}) => {
   return (
-    <div className="fixed inset-0 z-40 flex items-center justify-center w-100 min-h-screen" onClick={handleShowModal}>
+    <div className="fixed inset-0 z-40 top-0 left-0 flex items-center justify-center w-full h-full bg-gray-800 bg-opacity-50" onClick={handleShowModal}>
         <div className="bg-white p-6 md:p-6">
             <div className="w-screen md:w-96 md:pb-96 relative z-40">
                 <div className="absolute w-full h-full">
